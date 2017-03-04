b7be098322e6bc026dd5f5867ef87b53466221e1
Karol Sićko <karol.sicko@gmail.com>
2017-03-04 23:51:12 +0100

    war, thymeleaf, bootstrap

--- src/main/java/eu/karols/BlogApplication.java
+++ src/main/java/eu/karols/BlogApplication.java
@@ -3,13 +3,20 @@ package eu.karols;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
-public class BlogApplication {
-
+public class BlogApplication extends SpringBootServletInitializer {
+    
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BlogApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
